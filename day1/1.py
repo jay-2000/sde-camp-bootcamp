c2b754e2c0ff654f4fc1064c28d43bf3b448e099
jay-2000 <jayparmar7654321@gmail.com>
2022-02-02 23:57:24 +0530

    add code for day1

---
+++ day1/1.py
@@ -0,0 +1,6 @@
+# 1. Try some input and outputs(get character or int or double and print the output)
+
+fname = input("enter your name: ")
+lname = input("enter your surname: ")
+
+print("my name is",fname,lname)
---
+++ day1/2.py
@@ -0,0 +1,8 @@
+# 2. Check age is greater than 18(condition statement) 
+
+age = int(input("enter age : "))
+
+if age>18:
+	print("yes age is greater than 18")
+else:
+	print("no age is NOT greater than 18")
