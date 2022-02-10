217405291e37df1c16ca1ed59fd249fcca2dcb56
jay-2000 <jayparmar7654321@gmail.com>
2022-02-10 22:34:26 +0530

    add 1.py day5

---
+++ day5/1.py
@@ -0,0 +1,13 @@
+class Solution:
+	def permute(self, num):
+		if len(num)==0: return []
+		if len(num)==1: return [num]
+		res=[]
+		for i in range(len(num)):
+			for j in self.permute(num[:i]+num[i+1:]):
+				res.append([num[i]]+j)
+		return res
+
+
+if __name__=="__main__":
+	print Solution().permute([1,2,3])
